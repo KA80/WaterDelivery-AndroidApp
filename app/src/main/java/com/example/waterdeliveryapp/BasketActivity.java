8b2e0cbda28a888ce36ba445d67b5625b4d42595
Stanislav Nagornov <58802975+KA80@users.noreply.github.com>
2020-09-27 22:43:49 +1000

    BasketActivity.java & activity_basket.xml

--- app/src/main/java/com/example/waterdeliveryapp/BasketActivity.java
+++ app/src/main/java/com/example/waterdeliveryapp/BasketActivity.java
@@ -1,14 +1,64 @@
 package com.example.waterdeliveryapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BasketActivity extends AppCompatActivity {
+
+    private View.OnClickListener onOrderClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder builderOrder = new AlertDialog.Builder(BasketActivity.this);
+            builderOrder.setTitle("Confirm")
+                    .setMessage("Order description")
+                    .setPositiveButton("Agree", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            AlertDialog.Builder builderSuccess = new AlertDialog.Builder(
+                                    BasketActivity.this);
+                            builderSuccess.setTitle("Successful")
+                                    .setMessage("Order description")
+                                    .setPositiveButton("Ok", new DialogInterface
+                                            .OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                        }
+                                    })
+                                    .show();
+                        }
+                    })
+                    .setNegativeButton("Disagree", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    })
+                    .show();
+        }
+    };
+
+    private View.OnClickListener onAddClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent startAddToOrderActivity = new Intent(BasketActivity.this,
+                    AddToOrderActivity.class);
+            startActivity(startAddToOrderActivity);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+        Button orderButton = findViewById(R.id.btn_order);
+        Button addButton = findViewById(R.id.btn_add);
+
+        orderButton.setOnClickListener(onOrderClickListener);
+        addButton.setOnClickListener(onAddClickListener);
     }
 }
